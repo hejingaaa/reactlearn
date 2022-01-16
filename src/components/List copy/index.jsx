7f9015df1c08a27aa7fba174232bfc8a33a1c2b0
hejin <2363428016@qq.com>
2022-01-16 23:31:37 +0800

    第二次提交

--- src/components/List copy/index.jsx
+++ src/components/List copy/index.jsx
@@ -18,7 +18,7 @@ export default class List extends Component {
 			<ul className="todo-main">
 				{
 					todos.map( todo =>{
-						return <Item key={todo.id} {...todo} ={updateTodo} deleteTodo={deleteTodo}/>
+						return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>
 					})
 				}
 			</ul>
